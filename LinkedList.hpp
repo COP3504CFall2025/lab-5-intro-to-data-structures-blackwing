f27e5d3a3eb6b18e1fbe189d69506f0200edd698
Kaden Cameron <kadcam789@gmail.com>
2025-11-12 17:08:29 -0500

    probably gonna be 0 points

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -2,21 +2,29 @@
 #include <iostream>
 using namespace std;
 
-
+template<typename T>
+struct Node {
+	T data;
+	Node* prev;
+	Node* next;
+};
 
 template <typename T>
 class LinkedList {
+
 public:
+
 	// Behaviors
 	void printForward() const;
 	void printReverse() const;
 
 	// Accessors
 	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+
+	Node<T>* getHead();
+	const Node<T>* getHead() const;
+	Node<T>* getTail();
+	const Node<T>* getTail() const;
 
 	// Insertion
 	void addHead(const T& data);
@@ -39,10 +47,193 @@ public:
 
 private:
 	// Stores pointers to first and last nodes and count
-	Node* head;
-	Node* tail;
+	Node<T>* head;
+	Node<T>* tail;
 	unsigned int count;
 
 };
+
+template<typename T>
+LinkedList<T>::LinkedList() {
+	this->head = nullptr;
+	this->tail = nullptr;
+	this->count = 0;
+}
+
+template<typename T>
+LinkedList<T>::LinkedList(const LinkedList<T> &list) {
+	this->head = list.head;
+	this->tail = list.tail;
+	this->count = list.count;
+}
+
+template<typename T>
+LinkedList<T>::LinkedList(LinkedList<T> &&other) noexcept {
+	this->head = other.head;
+	this->tail = other.tail;
+	this->count = other.count;
+
+	other.head = nullptr;
+	other.tail = nullptr;
+	other.count = 0;
+}
+
+template<typename T>
+LinkedList<T>::~LinkedList() {
+	delete this->head;
+	this->head = nullptr;
+	delete this->tail;
+	this->tail = nullptr;
+	this->count = 0;
+}
+
+template<typename T>
+void LinkedList<T>::addHead(const T &data) {
+	count++;
+	Node<T>* node1 = new Node<T>();
+	node1->data = data;
+	node1->next = head;
+	head = node1;
+}
+
+template<typename T>
+void LinkedList<T>::addTail(const T &data) {
+	count++;
+	Node<T>* node2 = new Node<T>();
+	node2->data = data;
+	//node1->next = tail;
+	node2->prev = tail;
+	tail = node2;
+}
+
+template<typename T>
+bool LinkedList<T>::removeHead() {
+	if (head != nullptr) {
+		count--;
+		Node<T>* temp = head;
+		head = head->next;
+		delete temp;
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+bool LinkedList<T>::removeTail() {
+	if (tail != nullptr) {
+		count--;
+		Node<T>* temp = tail;
+		tail = tail->prev;
+		delete temp;
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+void LinkedList<T>::Clear() {
+	Node<T>* temp = head;
+	// while (temp != nullptr) {
+	// 	delete temp;
+	// 	temp = temp->next;
+	// }
+
+	for (int i = 0; i < count; i++) {
+		if (head->next != nullptr) {
+			temp = head->next;
+			delete head;
+			head = temp;
+		}
+	}
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getHead() {
+	return head;
+}
+
+template<typename T>
+const Node<T>* LinkedList<T>::getHead() const {
+	return head;
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getTail() {
+	return tail;
+}
+
+template<typename T>
+const Node<T>* LinkedList<T>::getTail() const {
+	return tail;
+}
+
+template<typename T>
+[[nodiscard]] unsigned int LinkedList<T>::getCount() const {
+	return count;
+}
+
+template<typename T>
+void LinkedList<T>::printForward() const {
+	for (Node<T>* tempNode = head; tempNode != nullptr; tempNode = tempNode->next) {
+		std::cout << tempNode->data << " " << std::endl;
+	}
+}
+
+template<typename T>
+void LinkedList<T>::printReverse() const {
+	for (Node<T>* tempNode = tail; tempNode != nullptr; tempNode = tempNode->prev) {
+		std::cout << tempNode->data << " " << std::endl;
+	}
+}
+
+template<typename T>
+LinkedList<T>& LinkedList<T>::operator=(LinkedList<T> &&other) noexcept {
+	if (this == &other) {
+		return *this;
+	}
+
+	delete this->head;
+	delete this->tail;
+
+	this->head = other.head;
+	this->tail = other.tail;
+	this->count = other.count;
+
+	other.head = nullptr;
+	other.tail = nullptr;
+	other.count = 0;
+
+	return *this;
+}
+
+template<typename T>
+LinkedList<T> &LinkedList<T>::operator=(const LinkedList<T> &rhs) {
+	if (this == &rhs) {
+		return *this;
+	}
+
+	delete this->head;
+	delete this->tail;
+
+	this->head = rhs.head;
+	this->tail = rhs.tail;
+	this->count = rhs.count;
+	return *this;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
