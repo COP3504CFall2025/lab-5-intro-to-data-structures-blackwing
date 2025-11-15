bec5c803b0ecf2aa6219997bd5e33be122d84322
Kaden Cameron <kadcam789@gmail.com>
2025-11-14 20:37:20 -0500

    I dont like dsa

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -53,16 +53,14 @@ public:
         if (list.getHead() == nullptr) {
             throw std::runtime_error("No elements in the stack");
         }
-        T item = list.getHead().data;
-        return item;
+        return list.getHead()->data;
     }
     const T& back() const override {
         if (list.getHead() == nullptr) {
             throw std::runtime_error("No elements in the stack");
         }
         //return list.getTail()->data;
-        T item = list.getTail().data;
-        return item;
+        return list.getTail()->data;
     }
 
     // Getter
